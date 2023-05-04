4ee8be922c19c128e43bdd544f08b886bad0ffcf
Susan Garcia <112519565+sggarcia6@users.noreply.github.com>
2023-05-04 13:57:01 -0400

    Update ff.R

--- code/ff.R
+++ code/ff.R
@@ -1,3 +1,22 @@
 #EXTRACTING FILES FROM FOLDER
 
 #FOLDER IS THE "agenome-ncbi
+
+
+install.packages("airspaceR")
+library(airspaceR)
+
+data <- read.faa("../agenome-ncbi_dataset/ncbi-dataset/data/GCF_900177485.1/protein.faa")
+
+
+#Protein files are different: 
+#1.
+#2.GCF_028230015.1
+#3.GCF_028230015.1
+#4.GCF_028229605.1
+#5.GCF_028229535.1
+#6.GCF_028221145.1
+#7.
+#8.
+#9.
+#10.
